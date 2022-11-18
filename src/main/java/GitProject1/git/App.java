e0b07a848ea545c0af5efbf5ede23a1fa71505ec
kravikumar <kravikumar@GBLNB-PG034KGC.galaxy.lan>
2022-11-18 15:43:42 +0530

    First Commit

---
+++ src/main/java/GitProject1/git/App.java
@@ -0,0 +1,13 @@
+package GitProject1.git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/git/TestGit.java
@@ -0,0 +1,12 @@
+package git;
+
+import org.testng.annotations.Test;
+
+public class TestGit {
+  @Test
+  public void f() {
+	  for(int i=1;i<=5;i++) {
+		  System.out.println("Hello Git");
+	  }
+  }
+}
